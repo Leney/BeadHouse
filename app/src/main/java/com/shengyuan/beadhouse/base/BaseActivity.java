579f0628d523abecf5c3a9c8c918df9c9fb2aefb
lenery <lilijunboy@163.com>
2017-10-31 10:40:48 +0800

    基础类  还未完善

--- app/src/main/java/com/shengyuan/beadhouse/base/BaseActivity.java
+++ app/src/main/java/com/shengyuan/beadhouse/base/BaseActivity.java
@@ -3,19 +3,51 @@ package com.shengyuan.beadhouse.base;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.RelativeLayout;
 
 import com.shengyuan.beadhouse.R;
+import com.shengyuan.beadhouse.gui.view.BaseTitleView;
 
 /**
  * Created by dell on 2017/10/30.
  */
 
 public abstract class BaseActivity extends AppCompatActivity {
+
+    protected RelativeLayout loadingLay;
+    protected RelativeLayout errorLay;
+    protected RelativeLayout emptyLay;
+    protected RelativeLayout contentLay;
+    protected BaseTitleView baseTitle;
+
+    private RelativeLayout.LayoutParams mParams = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.MATCH_PARENT);
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_base);
+
+        baseTitle = (BaseTitleView) findViewById(R.id.base_activity_title_view);
+        loadingLay = (RelativeLayout) findViewById(R.id.base_activity_loading_lay);
+        errorLay = (RelativeLayout) findViewById(R.id.base_activity_error_lay);
+        emptyLay = (RelativeLayout) findViewById(R.id.base_activity_empty_lay);
+        contentLay = (RelativeLayout) findViewById(R.id.base_activity_content_lay);
+
+        contentLay.addView(View.inflate(BaseActivity.this, getInitLayout(), null), mParams);
+        initView();
     }
 
     protected abstract int getInitLayout();
+
+    protected abstract void initView();
+
+
+    protected void showCenterView(){
+        loadingLay.setVisibility(View.GONE);
+        errorLay.setVisibility(View.GONE);
+        emptyLay.setVisibility(View.GONE);
+        contentLay.setVisibility(View.VISIBLE);
+    }
+
 }
--- app/src/main/java/com/shengyuan/beadhouse/gui/view/BaseTitleView.java
+++ app/src/main/java/com/shengyuan/beadhouse/gui/view/BaseTitleView.java
@@ -1,8 +1,159 @@
 package com.shengyuan.beadhouse.gui.view;
 
-/**
- * Created by dell on 2017/10/30.
- */
+import android.app.Activity;
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
-public class BaseTitleView {
+import com.shengyuan.beadhouse.R;
+
+
+/**
+ * 基本Title控件
+ * Created by yb on 2016/8/23.
+ */
+public class BaseTitleView extends FrameLayout {
+    private RelativeLayout allLay;
+    private ImageView leftImg;
+    private TextView titleName;
+    private ImageView rightImg;
+    private TextView titleRight;
+
+    public BaseTitleView(Context context) {
+        super(context);
+        init();
+    }
+
+    public BaseTitleView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public BaseTitleView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    private void init() {
+        View baseTitleView = View.inflate(getContext(), R.layout.base_title_view, null);
+        allLay = (RelativeLayout) baseTitleView.findViewById(R.id.base_title_lay);
+        leftImg = (ImageView) baseTitleView.findViewById(R.id.base_title_back_img);
+        titleName = (TextView) baseTitleView.findViewById(R.id.base_title_name);
+        rightImg = (ImageView) baseTitleView.findViewById(R.id.base_title_right_img);
+        titleRight = (TextView) baseTitleView.findViewById(R.id.base_title_right);
+        // 默认设置左边按钮是销毁当前界面
+        leftImg.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ((Activity) getContext()).finish();
+            }
+        });
+        addView(baseTitleView);
+    }
+
+    /**
+     * 设置标题名称
+     *
+     * @param title
+     */
+    public void setTitleName(String title) {
+        titleName.setText(title);
+    }
+
+    /**
+     * 设置标题背景颜色
+     *
+     * @param resId
+     */
+    public void setTitleBackground(int resId) {
+        allLay.setBackgroundResource(resId);
+    }
+
+
+    /**
+     * 设置标题名称字体颜色
+     *
+     * @param resId
+     */
+    public void setTitleNameColor(int resId) {
+        titleName.setTextColor(getResources().getColor(resId));
+    }
+
+    /**
+     * 设置返回按钮是否显示
+     *
+     * @param visible
+     */
+    public void setLeftImgVisible(boolean visible) {
+        leftImg.setVisibility(visible ? View.VISIBLE : View.GONE);
+    }
+
+    /**
+     * 设置右边图片的资源
+     *
+     * @param sourceId
+     */
+    public void setLeftImgRes(int sourceId) {
+        leftImg.setVisibility(VISIBLE);
+        leftImg.setImageResource(sourceId);
+    }
+
+    /**
+     * 设置左边按钮点击事件
+     *
+     * @param listener
+     */
+    public void setLeftImgOnClickListener(OnClickListener listener) {
+        leftImg.setOnClickListener(listener);
+    }
+
+    /**
+     * 设置右边图片的资源
+     *
+     * @param sourceId
+     */
+    public void setRightImgRes(int sourceId) {
+        rightImg.setVisibility(VISIBLE);
+        rightImg.setImageResource(sourceId);
+    }
+
+    /**
+     * 设置右边的图片点击事件
+     *
+     * @param listener
+     */
+    public void setRightImgClickListener(OnClickListener listener) {
+        rightImg.setOnClickListener(listener);
+    }
+
+    /**
+     * 设置右边的textView是否可见
+     *
+     * @param visible
+     */
+    public void setRightVisible(boolean visible) {
+        titleRight.setVisibility(visible ? View.VISIBLE : View.GONE);
+    }
+
+    /**
+     * 设置右边按钮点击事件
+     *
+     * @param listener
+     */
+    public void setRightOnClickListener(OnClickListener listener) {
+        titleRight.setOnClickListener(listener);
+    }
+
+    /**
+     * 设置右边textView的文本显示
+     *
+     * @param str
+     */
+    public void setRightText(String str) {
+        titleRight.setText(str);
+    }
 }
