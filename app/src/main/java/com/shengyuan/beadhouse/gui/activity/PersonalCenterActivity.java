7a1fb95ff5b25a023bccb90c741212225dc84fba
Lenery <lilijunboy@163.com>
2017-11-12 11:39:48 +0800

    个人中心界面

---
+++ app/src/main/java/com/shengyuan/beadhouse/gui/activity/PersonalCenterActivity.java
@@ -0,0 +1,20 @@
+package com.shengyuan.beadhouse.gui.activity;
+
+import com.shengyuan.beadhouse.base.BaseActivity;
+
+/**
+ * 个人中心
+ * Created by dell on 2017/11/12.
+ */
+
+public class PersonalCenterActivity extends BaseActivity {
+    @Override
+    protected int getLayoutId() {
+        return 0;
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+}
