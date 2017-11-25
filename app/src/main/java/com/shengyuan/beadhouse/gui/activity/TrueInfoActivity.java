47f5cc11393b1468785c1da49f558e16b945cad2
Lenery <lilijunboy@163.com>
2017-11-25 19:11:04 +0800

    填写真实信息界面更改

--- app/src/main/java/com/shengyuan/beadhouse/gui/activity/TrueInfoActivity.java
+++ app/src/main/java/com/shengyuan/beadhouse/gui/activity/TrueInfoActivity.java
@@ -2,8 +2,10 @@ package com.shengyuan.beadhouse.gui.activity;
 
 import android.content.Context;
 import android.content.Intent;
+import android.support.v4.content.ContextCompat;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.shengyuan.beadhouse.R;
@@ -17,7 +19,8 @@ import com.shengyuan.beadhouse.base.BaseActivity;
 public class TrueInfoActivity extends BaseActivity implements View.OnClickListener {
     private TextView account;
     private EditText nameInput;
-    private TextView sex;
+    private TextView man,woman;
+    private ImageView manSelect,womanSelect;
     private EditText cardNumInput;
     private TextView commitBtn;
 
@@ -31,10 +34,18 @@ public class TrueInfoActivity extends BaseActivity implements View.OnClickListen
         baseTitle.setTitleName(getResources().getString(R.string.true_name));
         account = (TextView) findViewById(R.id.true_info_account_text);
         nameInput = (EditText) findViewById(R.id.true_info_name_input);
-        sex = (TextView) findViewById(R.id.true_info_sex_text);
+        man = (TextView) findViewById(R.id.true_info_sex_man);
+        man.setOnClickListener(this);
+        woman = (TextView) findViewById(R.id.true_info_sex_woman);
+        woman.setOnClickListener(this);
+        manSelect = (ImageView) findViewById(R.id.true_info_man_select);
+        womanSelect = (ImageView) findViewById(R.id.true_info_woman_select);
         cardNumInput = (EditText) findViewById(R.id.true_info_card_num_input);
         commitBtn = (TextView) findViewById(R.id.true_info_commit_btn);
         commitBtn.setOnClickListener(this);
+
+        setSelectSex(0);
+
         showCenterView();
     }
 
@@ -44,6 +55,32 @@ public class TrueInfoActivity extends BaseActivity implements View.OnClickListen
             case R.id.true_info_commit_btn:
                 // 提交按钮
                 break;
+            case R.id.true_info_sex_man:
+                // 男
+                setSelectSex(0);
+                break;
+            case R.id.true_info_sex_woman:
+                // 女
+                setSelectSex(1);
+                break;
+        }
+    }
+
+    /**
+     * 设置显示性别选择的视图
+     * @param sex 0=男，1=女
+     */
+    private void setSelectSex(int sex){
+        if(sex == 0){
+            man.setTextColor(ContextCompat.getColor(TrueInfoActivity.this,R.color.main_color));
+            woman.setTextColor(ContextCompat.getColor(TrueInfoActivity.this,R.color.text_666666));
+            manSelect.setVisibility(View.VISIBLE);
+            womanSelect.setVisibility(View.GONE);
+        }else {
+            man.setTextColor(ContextCompat.getColor(TrueInfoActivity.this,R.color.text_666666));
+            woman.setTextColor(ContextCompat.getColor(TrueInfoActivity.this,R.color.main_color));
+            manSelect.setVisibility(View.GONE);
+            womanSelect.setVisibility(View.VISIBLE);
         }
     }
 
